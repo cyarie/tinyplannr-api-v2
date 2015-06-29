73a4d2512c6f5ba8aba8bdbe60773acc901b1544
Chris Yarie <cyarie@gmail.com>
2015-06-29 00:19:01 -0400

    Mah gawd, it's working tests for getting a user

--- handlers/indexHandler.go
+++ handlers/indexHandler.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	_ "github.com/lib/pq"
 	"github.com/cyarie/tinyplannr-api-v2/settings"
 )
 
-func IndexHandler(a *settings.AppContext, w http.ResponseWriter, r *http.Request) (int, error) {
+func IndexHandler(ac *settings.AppContext, w http.ResponseWriter, r *http.Request) (int, error) {
 	fmt.Fprint(w, "WELCOME TO GORT")
+
+	ac.HandlerResp = http.StatusOK
 	return 200, nil
 }
--- settings/appContext.go
+++ settings/appContext.go
@@ -1,14 +1,18 @@
 package settings
 
 import (
-	"database/sql"
-
 	"github.com/gorilla/securecookie"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type AppContext struct {
-	Db					*sql.DB
+	Db					*sqlx.DB
 	CookieMachine		*securecookie.SecureCookie
 	HandlerResp			int
+}
+
+type JsonErr struct {
+	Code		int		`json:"code"`
+	Text		string	`json:"error"`
 }
